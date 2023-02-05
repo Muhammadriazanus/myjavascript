cd57928330ab7de59f196404c592980d72c7b5e1
muhammadriazanus <riazanus568@gmail.com>
2023-02-05 10:16:40 -0800

    programing findamental

--- assigement3part2.js
+++ assigement3part2.js
@@ -22,4 +22,33 @@ var esmtiamtedMaxium  = 65;
 var amountOfSnacksPerDay = 3;
 var average = currentAge+esmtiamtedMaxium/2 * 3;
 console.log(average)
-alert("your will be need " + " "+ "150 "  +   favourirteSnack   + "last you unitil the recipe " + esmtiamtedMaxium)
+alert("your will be need " + " "+ "150 "  +   favourirteSnack   + " last you unitil the recipe " + esmtiamtedMaxium)
+// assigment on 6 math expression 
+
+// var a =11
+// document.write(++a)
+// var a  = 11
+// document.write(--a)
+// question no  2
+let a  = prompt("entr a value of a")
+let   b = prompt("Enter a number of b")
+let result  = --a - --b + ++b + b++
+alert(result)
+let userName = "muhammad anas"
+let greetUser = "welcome to syalni welfare center"
+document.write(`${userName}  ${greetUser}`)
+//  question no 3
+let j = 5
+for(i = 1 ; i<=10;i++){
+    document.write(`${j} * ${i} = ${j*i} <br>`)
+}
+// QUESTION NO 6
+let suject3 = prompt("urdu") 
+let suject1 = prompt("eng") 
+let suject2 = prompt("hied") 
+let total = 300
+let obtainedMark = 90
+let totalSubject = "suject1+suject2+suject3"
+if(totalSubject==obtainedMark){
+    alert(obtainedMark/300*100)
+}
